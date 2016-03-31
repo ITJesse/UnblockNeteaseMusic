59c35f73def3be7e51511ee9b103bb228262dba1
Jesse Zhu <jesse@itjesse.cn>
2016-03-31 19:40:53 +0800

    error handle
    get songs from kugou
    
    fix download api
    
    fix Unexpected token u
    
    fix no resource log
    
    update version

--- routes/index.js
+++ routes/index.js
@@ -2,122 +2,163 @@ var express = require('express');
 var router = express.Router();
 var Utils = require('../utils');
 var colors = require('colors');
-var request = require('request');
+var waterfall = require("async/waterfall");
 
 var utils = new Utils();
 
-router.get('/*', function(req, res) {
-  var url = req.originalUrl;
-  if(!/^http/.test(url)){
-    url = "http://223.252.199.7" + url;
-  }
-  var options = {
-    url: url,
-    headers: req.headers,
-    method: 'get'
-  };
-  request(options)
-    .on('error', function(err) {
-      console.error(err)
-    })
-    .pipe(res);
-});
-
-router.post('/*', function(req, res, next) {
-  if (/mp3$|flac$/.test(req.originalUrl)) {
-    return request.get(req.originalUrl).pipe(res);
-  }
-  utils.defaultPost(req.headers, req.body, req.originalUrl, function(err, headers, body) {
-    if (err) {
-      return console.error(err);
-    } else {
-      // console.log(body);
-      // console.log(utils);
-      next();
-    }
-  });
-});
-
 router.post('/eapi/v3/song/detail', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/v3/playlist/detail', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/v1/album/*', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/batch', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/cloudsearch/pc', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/v1/artist', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/batch', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/v1/search/get', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/song/enhance/privilege', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/v1/discovery/new/songs', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 router.post('/eapi/v1/play/record', function(req, res, next) {
-  utils.modifyDetailApi();
+  res.defaultBody = utils.netease.modifyDetailApi(res.defaultBody);
   next();
 });
 
 router.post('/eapi/song/enhance/player/url', function(req, res, next) {
-  if (utils.getPlaybackReturnCode() != 200 || utils.getPlaybackBitrate() < 320000) {
-    utils.modifyPlayerApi(function(err) {
-      if (err) {
-        return console.error(err.red);
-      } else {
+  if (utils.netease.getPlaybackReturnCode(res.defaultBody) != 200 || utils.netease.getPlaybackBitrate(res.defaultBody) < 320000) {
+    waterfall([
+      function(callback) {
+        var songId = utils.netease.getPlaybackSongId(res.defaultBody);
+        callback(null, songId);
+      },
+      function(songId, callback) {
+        utils.netease.getSongDetail(songId, function(err, detail) {
+          if (err) {
+            console.error(err);
+          } else {
+            callback(null, detail);
+          }
+        });
+      },
+      function(detail, callback) {
+        var songName = utils.netease.getSongName(detail);
+        var artist = utils.netease.getArtistName(detail);
+        utils.kugou.search(songName, artist, function(err, hash, bitrate) {
+          if (err) {
+            console.error(err);
+          } else {
+            callback(null, hash, bitrate);
+          }
+        });
+      },
+      function(hash, bitrate, callback) {
+        utils.kugou.getUrl(hash, function(err, url) {
+          if (err) {
+            console.error(err);
+          } else {
+            callback(null, url, bitrate);
+          }
+        });
+      }
+    ], function(err, url, bitrate) {
+      if(err) {
+        console.error(err);
+        res.status = 500;
+        res.send();
+      }else{
+        res.defaultBody = utils.netease.modifyPlayerApiCustom(url, bitrate, res.defaultBody);
         next();
       }
     });
   } else {
-    console.log('Playback bitrate is not changed. The song URL is '.green + utils.getPlaybackUrl().green);
+    console.log('Playback bitrate is not changed. The song URL is '.green + utils.netease.getPlaybackUrl(res.defaultBody).green);
     next();
   }
 });
 
 router.post('/eapi/song/enhance/download/url', function(req, res, next) {
-  if (utils.getDownloadReturnCode() != 200 || utils.getDownloadBitrate() < 320000) {
-    utils.modifyDownloadApi(function(err) {
-      if (err) {
-        return console.error(err.red);
-      } else {
+  if (utils.netease.getDownloadReturnCode(res.defaultBody) != 200 || utils.netease.getDownloadBitrate(res.defaultBody) < 320000) {
+    waterfall([
+      function(callback) {
+        var songId = utils.netease.getDownloadSongId(res.defaultBody);
+        callback(null, songId);
+      },
+      function(songId, callback) {
+        utils.netease.getSongDetail(songId, function(err, detail) {
+          if (err) {
+            console.error(err);
+          } else {
+            callback(null, detail);
+          }
+        });
+      },
+      function(detail, callback) {
+        var songName = utils.netease.getSongName(detail);
+        var artist = utils.netease.getArtistName(detail);
+        utils.kugou.search(songName, artist, function(err, hash, bitrate) {
+          if (err) {
+            console.error(err);
+          } else {
+            callback(null, hash, bitrate);
+          }
+        });
+      },
+      function(hash, bitrate, callback) {
+        utils.kugou.getUrl(hash, function(err, url) {
+          if (err) {
+            console.error(err);
+          } else {
+            callback(null, url, bitrate);
+          }
+        });
+      }
+    ], function(err, url, bitrate) {
+      if(err) {
+        console.error(err);
+        res.status = 500;
+        res.send();
+      }else{
+        res.defaultBody = utils.netease.modifyDownloadApiCustom(url, bitrate, res.defaultBody);
         next();
       }
     });
   } else {
-    console.log('Download bitrate is not changed. The song URL is '.green + utils.getPlaybackUrl().green);
+    console.log('Download bitrate is not changed. The song URL is '.green + utils.netease.getDownloadUrl(res.defaultBody).green);
     next();
   }
 });
 
 router.all('/*', function(req, res, next) {
   // console.log(utils.headers);
-  // console.log(utils.body);
-  res.set(utils.headers);
-  res.send(utils.body);
+  // console.log(res.defaultBody);
+  res.send(res.defaultBody);
 });
 
 module.exports = router;
---
+++ utils/kugou/index.js
@@ -0,0 +1,60 @@
+var colors = require('colors');
+var md5 = require('md5');
+var request = require('request');
+
+var kugou = function() {};
+
+kugou.prototype.search = function(name, artist, callback) {
+  console.log("Song name: ".green + name);
+  console.log("Artist: ".green + artist);
+  var songName = encodeURIComponent(artist + " " + name);
+  var url = "http://mobilecdn.kugou.com/api/v3/search/song?format=json&keyword=" + songName + "&page=1&pagesize=1&showtype=1";
+  request.get(url, function(err, res, body) {
+    if (err) {
+      console.error(err);
+      callback(err);
+    } else {
+      var data = JSON.parse(body);
+      if (data.status == 1 && !!data['data']['info'].length) {
+        if (data['data']['info'][0]['songname'].indexOf(name) != -1) {
+          var hash320 = data['data']['info'][0]['320hash'];
+          var hash128 = data['data']['info'][0]['hash'];
+          if (!!hash320.length) {
+            var hash = hash320;
+            var bitrate = 320000;
+          } else {
+            var hash = hash128;
+            var bitrate = 128000;
+          }
+          callback(null, hash, bitrate);
+        } else {
+          callback('No resource found on kugou.');
+        }
+      } else {
+        callback('No resource found on kugou.');
+      }
+    }
+  });
+}
+
+kugou.prototype.getUrl = function(hash, callback) {
+  var key = md5(hash + 'kgcloud');
+  var url = "http://trackercdn.kugou.com/i/?acceptMp3=1&cmd=4&pid=6&hash=" + hash + "&key=" + key;
+  request.get(url, function(err, res, body) {
+    if (err) {
+      console.error(err);
+      callback(err);
+    } else {
+      var data = JSON.parse(body);
+      if (data.status == 1) {
+        var url = data['url'];
+        callback(null, url);
+      } else {
+        console.error(data['error']);
+        callback(data['error']);
+      }
+    }
+  });
+}
+
+module.exports = kugou;
