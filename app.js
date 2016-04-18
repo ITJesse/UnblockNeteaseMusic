960b4244806f257f6307e5be30964a8026fa63e1
Jesse Zhu <jesse@itjesse.cn>
2016-04-18 15:03:45 +0800

    switch to koa

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-var express = require('express');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
-var proxy = require('./middleware')
+var app = require('koa')();
+var logger = require('koa-logger');
+var proxy = require('./proxy');
 
-var routes = require('./routes/index');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(bodyParser.raw({
-  type: 'application/x-www-form-urlencoded'
-}));
+app.use(logger());
 app.use(proxy);
-
-app.use('/', routes);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.use(function(err, req, res, next) {
-  console.error(err);
-  res.status(err.status || 500);
-  res.send({
-    message: err.message,
-    error: {}
-  });
-});
-
 
 module.exports = app;
