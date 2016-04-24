2a3706889ec549a36c7a96a223319e85b80bc26d
Jesse Zhu <jesse@itjesse.cn>
2016-04-25 00:20:04 +0800

    允许使用低位端口

--- modules/config/index.js
+++ modules/config/index.js
@@ -6,11 +6,11 @@ program
   .option('-f, --force-ip <ip>', 'Force the netease server ip.')
   .parse(process.argv);
 
-if(program.port && (program.port < 1000 || program.port > 65535)){
-  console.log('Port must be higher than 1000 and lower than 65535.'.red);
+if (program.port && (program.port < 1 || program.port > 65535)) {
+  console.log('Port must be higher than 0 and lower than 65535.'.red);
   process.exit(1);
 }
-if(program.forceIp && !/\d+\.\d+\.\d+\.\d+/.test(program.forceIp)){
+if (program.forceIp && !/\d+\.\d+\.\d+\.\d+/.test(program.forceIp)) {
   console.log('Please check the ip address.'.red);
   process.exit(1);
 }
