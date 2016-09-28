730671cba128daea641f42be376529482c400246
Jesse Zhu <jesse@itjesse.cn>
2016-09-29 04:09:19 +0800

    Fix QQ Music module

--- src/modules/utils/kugou/index.js
+++ src/modules/utils/kugou/index.js
@@ -17,26 +17,26 @@ export default class kugou {
     return new Promise(async function(resolve, reject) {
       try {
         let result = await common.sendRequest(options);
+        let data = JSON.parse(result[1]);
+        if (data.status == 1 &&
+            !!data['data']['info'].length &&
+            !!data['data']['info'][0]['320hash'].length &&
+            data['data']['info'][0]['songname'].indexOf(name) != -1) {
+
+          let hash320 = data['data']['info'][0]['320hash'];
+          result = {
+            hash: hash320,
+            bitrate: 320000,
+            filesize: data['data']['info'][0]['320filesize']
+          };
+          return resolve(result);
+        } else {
+          console.error('No resource found on kugou.'.yellow);
+          return resolve(null);
+        }
       } catch(err) {
         console.log(err);
         return reject(err);
-      }
-      let data = JSON.parse(result[1]);
-      if (data.status == 1 &&
-          !!data['data']['info'].length &&
-          !!data['data']['info'][0]['320hash'].length &&
-          data['data']['info'][0]['songname'].indexOf(name) != -1) {
-
-        let hash320 = data['data']['info'][0]['320hash'];
-        result = {
-          hash: hash320,
-          bitrate: 320000,
-          filesize: data['data']['info'][0]['320filesize']
-        };
-        return resolve(result);
-      } else {
-        console.error('No resource found on kugou.'.yellow);
-        return resolve(null);
       }
     });
   };
@@ -50,23 +50,23 @@ export default class kugou {
     return new Promise(async function(resolve, reject) {
       try {
         let result = await common.sendRequest(options);
+        let data = JSON.parse(result[1]);
+        if (data.status == 1) {
+          let url = data['url'];
+
+          // 魔改 URL 应对某司防火墙
+          if(config.rewriteUrl) {
+            url = url.replace('fs.web.kugou.com', 'music.163.com/kugou');
+          }
+
+          return resolve(url);
+        } else {
+          console.error(data['error']);
+          return reject(data['error']);
+        }
       } catch(err) {
         console.log(err);
         return reject(err);
-      }
-      let data = JSON.parse(result[1]);
-      if (data.status == 1) {
-        let url = data['url'];
-
-        // 魔改 URL 应对某司防火墙
-        if(config.rewriteUrl) {
-          url = url.replace('fs.web.kugou.com', 'music.163.com/kugou');
-        }
-
-        return resolve(url);
-      } else {
-        console.error(data['error']);
-        return reject(data['error']);
       }
     });
   };
--- src/modules/utils/qq/index.js
+++ src/modules/utils/qq/index.js
@@ -2,11 +2,56 @@ import colors from 'colors';
 import md5 from 'md5';
 import request from 'request';
 import common from '../common';
+import config from '../../config';
 
 let qq = function() {};
 
 export default class QQ {
+  constructor() {
+    this.guid = this.getGUid();
+
+    let self = this;
+    this.getVKey()
+      .then((vkey) => {
+        self.vkey = vkey;
+        console.log("QQ Music module is ready.".green);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getGUid() {
+    let currentMs = parseInt((new Date()).valueOf() % 1000);
+    return parseInt(Math.round(Math.random() * 2147483647) * currentMs % 0x1E10);
+  }
+
+  getVKey() {
+    let self = this;
+    let options = {
+      url: "http://base.music.qq.com/fcgi-bin/fcg_musicexpress.fcg?json=3&guid=" + self.guid
+    };
+
+    return new Promise(async function(resolve, reject) {
+      try {
+        let result = await common.sendRequest(options);
+        result[1] = result[1]
+          .replace(/^jsonCallback\(/, '')
+          .replace(/\);$/, '');
+        let data = JSON.parse(result[1]);
+        return resolve(data.key);
+      }catch(err) {
+        console.log(err);
+        return reject(err);
+      };
+    });
+  };
+
   search(name, artist) {
+    let self = this;
+    if(self.vkey.length != 112) {
+      return console.log("QQ Music module is not ready.".red);
+    }
     console.log("Search from QQ Music.".green);
     console.log("Song name: ".green + name);
     console.log("Artist: ".green + artist);
@@ -26,9 +71,30 @@ export default class QQ {
             fsong.indexOf(keyword) != -1) {
 
           let list = data.data.song.list[0].f.split('|');
+          let mid = list[20];
+          let bitrate = list[13];
+          let prefix,
+              ext;
+          if(bitrate == '320000') {
+            prefix = 'M800';
+            ext = 'mp3';
+          }
+          else if(bitrate == '128000') {
+            prefix = 'M500';
+            ext = 'mp3';
+          }
+          else {
+            prefix = 'C200';
+            ext = 'm4a';
+          }
+          let url = "http://cc.stream.qqmusic.qq.com/" + prefix + mid + "." + ext + "?vkey=" + self.vkey + "&guid=" + self.guid + "&fromtag=0";
+          // 魔改 URL 应对某司防火墙
+          if(config.rewriteUrl) {
+            url = url.replace('cc.stream.qqmusic.qq.com', 'music.163.com/qqmusic');
+          }
           result = {
-            url: 'http://music.163.com/qqmusic/' + list[0] + '.mp3',
-            bitrate: list[13],
+            url: url,
+            bitrate: bitrate,
             filesize: list[11],
             hash: md5(list[0])
           };
@@ -39,7 +105,7 @@ export default class QQ {
         }
       }catch(err) {
         console.log(err);
-        reject(err);
+        return reject(err);
       };
     });
   };
