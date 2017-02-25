7765f52a9bbecc33dc2cd7a813a1db00aeb3673c
Jesse Zhu <jesse@itjesse.cn>
2017-02-26 05:00:37 +0900

    delete useless command line params

--- src/modules/config/index.js
+++ src/modules/config/index.js
@@ -4,8 +4,6 @@ import 'colors';
 program
   .option('-p, --port <port>', 'Specific server port.')
   .option('-f, --force-ip <ip>', 'Force the netease server ip.')
-  .option('-k, --kugou', 'Find copyright music on Kugou.')
-  .option('-q, --qq', 'Find copyright music on QQ Music.')
   .option('-r, --rewrite-url', 'Rewrite music download url, let client download file through proxy.')
   .parse(process.argv);
 
@@ -20,15 +18,6 @@ if (program.forceIp && !/\d+\.\d+\.\d+\.\d+/.test(program.forceIp)) {
 
 if (program.rewriteUrl) {
   console.log('Rewrite music download url.'.green);
-}
-if (program.kugou) {
-  console.log('Finding copyright music on Kugou.'.green);
-}
-if (program.qq) {
-  console.log('Finding copyright music on QQ Music.'.green);
-}
-if (!program.kugou && !program.qq) {
-  console.log('Proxy will do nothing with copyright music.'.yellow);
 }
 
 export default program;
