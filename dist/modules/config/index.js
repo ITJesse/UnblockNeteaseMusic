caab4f38813e64ac61eb14fe5dc993b877aeaff3
Jesse Zhu <jesse@itjesse.cn>
2017-03-02 03:33:19 +0900

    build & bump version

--- dist/modules/config/index.js
+++ dist/modules/config/index.js
@@ -18,7 +18,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 (0, _pkginfo2.default)(module);
 
-_commander2.default.version(module.exports.version).option('-p, --port <port>', 'Specific server port.').option('-f, --force-ip <ip>', 'Force the netease server ip.').option('-r, --rewrite-url', 'Rewrite music download url, let client download file through proxy.').parse(process.argv);
+_commander2.default.version(module.exports.version).option('-p, --port <port>', 'Specific server port.').option('-f, --force-ip <ip>', 'Force the netease server ip.').option('-k, --kugou', 'No longer support.').option('-q, --qq', 'No longer support.').option('-r, --rewrite-url', 'Rewrite music download url, let client download file through proxy.').parse(process.argv);
 
 if (_commander2.default.port && (_commander2.default.port < 1 || _commander2.default.port > 65535)) {
   console.log('Port must be higher than 0 and lower than 65535.'.red);
