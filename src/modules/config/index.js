d239767f2a4b1a59abefbeb29dfcafaedb76f84c
Jesse Zhu <jesse@itjesse.cn>
2017-03-05 04:10:14 +0900

    add notice on deprecated options

--- src/modules/config/index.js
+++ src/modules/config/index.js
@@ -13,10 +13,15 @@ program
   .option('-r, --rewrite-url', 'Rewrite music download url, let client download file through proxy.')
   .parse(process.argv);
 
+if (program.kugou || program.qq) {
+  console.log('The option --kugou and --qq is no longer support.'.yellow);
+}
+
 if (program.port && (program.port < 1 || program.port > 65535)) {
   console.log('Port must be higher than 0 and lower than 65535.'.red);
   process.exit(1);
 }
+
 if (program.forceIp && !/\d+\.\d+\.\d+\.\d+/.test(program.forceIp)) {
   console.log('Please check the ip address.'.red);
   process.exit(1);
