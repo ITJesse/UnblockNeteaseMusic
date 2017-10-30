8586e039ae4a75cb7e030facd7d326241d2c5eb6
Jesse Zhu <jesse@itjesse.com>
2017-10-31 06:10:14 +0900

    npm ignore

--- dist/bin/unblockneteasemusic.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-var version = process.version.replace('v', '').split('.');
-if (version[0] < 8) {
-  console.log('Unsupported nodejs version ' + process.version + '. Please upgrade.');
-} else {
-  require('./run');
-}
